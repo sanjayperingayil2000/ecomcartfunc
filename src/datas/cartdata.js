5c8e4ba60a31a3d0a6581048bd002b8c4400084d
sanjayperingayil2000 <109661238+sanjayperingayil2000@users.noreply.github.com>
2023-02-19 23:33:01 +0530

    Update cartdata.js

--- src/datas/cartdata.js
+++ src/datas/cartdata.js
@@ -1,8 +1,5 @@
 export const cartdata=[
     {img: 'https://avatars.mds.yandex.net/get-mpic/5236248/img_id3611871067916804795.jpeg/orig', 
     name: 'Apple pencil', 
-    offer: '7000'},
-    {img: 'https://avatars.mds.yandex.net/get-mpic/5236248/img_id3611871067916804795.jpeg/orig', 
-    name: 'Apple pencil',  
-    offer: '8000'}
+    offer: '7000'}
 ]
