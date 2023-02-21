a51e789e145581477ec78bef0dc5e77c60f5b4b4
sanjayperingayil2000 <109661238+sanjayperingayil2000@users.noreply.github.com>
2023-02-21 14:41:03 +0530

    Update cartdata.js

--- src/datas/cartdata.js
+++ src/datas/cartdata.js
@@ -1,5 +1,6 @@
 export const cartdata=[
     {img: 'https://avatars.mds.yandex.net/get-mpic/5236248/img_id3611871067916804795.jpeg/orig', 
     name: 'Apple pencil', 
-    offer: '7000'}
+    offer: '7000',
+    qty:1}
 ]
